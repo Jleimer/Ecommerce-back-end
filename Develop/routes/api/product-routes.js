afe64cea1167eaa80675d3238c02a46e6d39668c
Jonathan <eimerarts@gmail.com>
2020-08-22 22:29:43 -0500

    Almost done with CRUD

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,26 +5,59 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+
+    ],
+    include: [
+      {
+        model: Category
+      },
+       {
+        model: Tag
+       }
+    ]
+  })
 });
 
 // get one product
 router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock'
+
+    ],
+    include: [
+      {
+        model: Category
+      },
+       {
+        model: Tag
+       }
+    ]
+  })
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
+  Product.create(
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      tagIds: req.body.tagIds
+    })
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
